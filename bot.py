652212e590fa4313cc6b8436e962c9597e8a9a66
Elk Cloner <28754537+elkcl@users.noreply.github.com>
2020-11-20 22:51:29 +0300

    temp

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ result, data = mail.uid('search', None, "ALL")
 last_uid = data[0].split()[-1]
 print('Ready!')
 print('Waiting...')
-time.sleep(300)
+time.sleep(90)
 
 while True:
     print('Checking...')
@@ -79,10 +79,11 @@ while True:
         continue
                     
     print('Waiting...')
-    hrs = datetime.now(tz).hour
-    if hrs >= 22:
-        time.sleep((30-hrs)*3600)
-    elif hrs <= 5:
-        time.sleep((6-hrs)*3600)
-    else:
-        time.sleep(300)
+    time.sleep(90)
+    #hrs = datetime.now(tz).hour
+    #if hrs >= 22:
+    #    time.sleep((30-hrs)*3600)
+    #elif hrs <= 5:
+    #    time.sleep((6-hrs)*3600)
+    #else:
+    #    time.sleep(300)

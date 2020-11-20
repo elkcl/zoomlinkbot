ba71ec9d3a3588646e8138bdec7266866028eb04
Elk Cloner <28754537+elkcl@users.noreply.github.com>
2020-11-20 09:53:38 +0300

    eof error fix

--- bot.py
+++ bot.py
@@ -39,36 +39,42 @@ time.sleep(300)
 
 while True:
     print('Checking...')
-    result, data = mail.uid('search', None, "ALL")
-    uidList = data[0].split()
-    curr_uid = uidList[-1]
-    if curr_uid != last_uid:
-        start_index = uidList.index(last_uid)
-        if start_index == -1:
-            start_index = len(uidList) - 1
-        last_uid = curr_uid
-        for i in range(start_index, len(uidList)):
-            result, data = mail.uid('fetch', uidList[i], '(RFC822)')
-            raw_email = data[0][1]
-            email_message = email.message_from_bytes(raw_email)
-            from_addr = email.utils.parseaddr(email_message['From'])[1]
-            contents = base64.b64decode(get_first_text_block(email_message)).decode('utf-8')
-            urls = re.findall(r'(https?://[^\s]+)', contents)
-            zoom_link = ""
-            found = False
-            for url in urls:
-                ex = tldextract.extract(url)
-                if ex.domain == 'zoom' and ex.suffix == 'us':
-                    zoom_link = url
-                    found = True
-                    break
-            if found:
-                print('Found!')
-                for row, addrs in enumerate(wk.get_col(4)):
-                    if addrs.find(from_addr) != -1:
-                        wk.cell((row+1, 2)).set_value(zoom_link)
-                        wk.cell((row+1, 3)).set_value(datetime.now(tz).strftime("%H:%M %b-%d"))
+    try:
+        result, data = mail.uid('search', None, "ALL")
+        uidList = data[0].split()
+        curr_uid = uidList[-1]
+        if curr_uid != last_uid:
+            start_index = uidList.index(last_uid)
+            if start_index == -1:
+                start_index = len(uidList) - 1    
+            for i in range(start_index, len(uidList)):
+                result, data = mail.uid('fetch', uidList[i], '(RFC822)')
+                raw_email = data[0][1]
+                email_message = email.message_from_bytes(raw_email)
+                from_addr = email.utils.parseaddr(email_message['From'])[1]
+                contents = base64.b64decode(get_first_text_block(email_message)).decode('utf-8')
+                urls = re.findall(r'(https?://[^\s]+)', contents)
+                zoom_link = ""
+                found = False
+                for url in urls:
+                    ex = tldextract.extract(url)
+                    if ex.domain == 'zoom' and ex.suffix == 'us':
+                        zoom_link = url
+                        found = True
                         break
+                if found:
+                    print('Found!')
+                    for row, addrs in enumerate(wk.get_col(4)):
+                        if addrs.find(from_addr) != -1:
+                            wk.cell((row+1, 2)).set_value(zoom_link)
+                            wk.cell((row+1, 3)).set_value(datetime.now(tz).strftime("%H:%M %b-%d"))
+                            break
+            last_uid = curr_uid
+        except imaplib.IMAP4.abort as e:
+            mail = imaplib.IMAP4_SSL("imap.mail.ru")
+            mail.login(credentials["login"], credentials["password"])
+            mail.select('INBOX')
+            continue
                     
     print('Waiting...')
     hrs = datetime.now(tz).hour
